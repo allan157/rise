ae4033a3dfc7b2eaf918c397b5a0e41c595bb945
allan157 <allanwafula2017@gmail.com>
2024-01-18 15:22:53 +0300

    1

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:rise/widgets/custom_button.dart';
+
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Column(
+        mainAxisAlignment: MainAxisAlignment.center ,
+        children: [
+          const Text("Start or Join a meeting", 
+          style: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          )
+          ), 
+          Image.asset('lib/assets/images/onboarding.jpg'),
+          CustomButton(text: 'Login',)
+      ],
+      ),
+    );
+  }
+}
